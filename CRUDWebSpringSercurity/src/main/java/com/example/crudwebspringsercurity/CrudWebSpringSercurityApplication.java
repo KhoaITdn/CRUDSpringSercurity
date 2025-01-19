47e3a386b61063993adf8f0f235b20208aafe96f
KhoaITdn <166222072+KhoaITdn@users.noreply.github.com>
2025-01-19 21:33:40 +0700

    Add files via upload

---
+++ CRUDWebSpringSercurity/src/main/java/com/example/crudwebspringsercurity/CrudWebSpringSercurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudwebspringsercurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWebSpringSercurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWebSpringSercurityApplication.class, args);
+	}
+
+}
---
+++ CRUDWebSpringSercurity/src/test/java/com/example/crudwebspringsercurity/CrudWebSpringSercurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudwebspringsercurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebSpringSercurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
